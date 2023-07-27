8157e7d4574f39a1f293f3e07edd789a82e0814f
WelcomeParac <Aaronhardyofficial@gmail.com>
2023-07-27 12:01:15 -0700

    Update clock functionality
    clock now display dates for each city

--- script.js
+++ script.js
@@ -1,123 +1,182 @@
-function updateClock(cityId, offset) {
-  const now = new Date();
-  const utcOffset = offset * 60 * 60 * 1000; // Convert hours to milliseconds
-  const cityTime = new Date(now.getTime() + utcOffset);
-  const hours = cityTime.getUTCHours() % 12 || 12;
-  const minutes = String(cityTime.getUTCMinutes()).padStart(2, '0');
-  const amPm = cityTime.getUTCHours() >= 12 ? 'PM' : 'AM';
-  const clockDiv = document.getElementById(cityId);
-  clockDiv.textContent = `${hours}:${minutes} ${amPm}`;
+function updateClock( cityId, offset ) {
+	const now = new Date();
+	const utcOffset = offset * 60 * 60 * 1000;
+	const cityTime = new Date( now.getTime() + utcOffset );
+	const hours = cityTime.getUTCHours() % 12 || 12;
+	const minutes = String( cityTime.getUTCMinutes() ).padStart( 2, '0' );
+	const amPm = cityTime.getUTCHours() >= 12 ? 'PM' : 'AM';
+	const day = cityTime.getUTCDate();
+	const month = cityTime.getUTCMonth() + 1;
+	const clockDiv = document.getElementById( cityId );
+	clockDiv.textContent = `${hours}:${minutes} ${amPm} / ${month}-${day}`;
 }
 
-// Function to update the user's local time
 function updateUserLocationClock() {
-  const now = new Date();
-  const hours = now.getHours() % 12 || 12;
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const amPm = now.getHours() >= 12 ? 'PM' : 'AM';
-  const clockDiv = document.getElementById('user-location');
-  clockDiv.textContent = `${hours}:${minutes} ${amPm}`;
+	const now = new Date();
+	const hours = now.getHours() % 12 || 12;
+	const minutes = String( now.getMinutes() ).padStart( 2, '0' );
+	const amPm = now.getHours() >= 12 ? 'PM' : 'AM';
+	const day = now.getDate();
+	const month = now.getMonth() + 1;
+	const year = now.getFullYear();
+	const clockDiv = document.getElementById( 'user-location' );
+	clockDiv.textContent = `You ➔ ${hours}:${minutes} ${amPm}  / ${month}-${day}-${year}`;
 }
 
-function highlightCity(city) {
-  const cityElements = document.querySelectorAll('.clock');
-  cityElements.forEach((element) => {
-    const cityName = element.parentElement.textContent.trim();
-    const cityDiv = element.parentElement;
-    if (cityName.toLowerCase().includes(city.toLowerCase())) {
-      cityDiv.classList.add('highlighted', 'zoomed', 'centered');
-    } else {
-      cityDiv.classList.add('faded');
-    }
-    setTimeout(() => {
-      cityDiv.classList.remove('highlighted', 'faded', 'centered');
-    }, 5000);
-  });
+function highlightCity( city ) {
+	const cityElements = document.querySelectorAll( '.clock' );
+	cityElements.forEach( ( element ) => {
+		const cityName = element.parentElement.textContent.trim();
+		const cityDiv = element.parentElement;
+		if ( cityName.toLowerCase().includes( city.toLowerCase() ) ) {
+			cityDiv.classList.add( 'highlighted' );
+		} else {}
+		setTimeout( () => {
+			cityDiv.classList.remove( 'highlighted' );
+		}, 3000 );
+	} );
 }
 
-function startClocks() {
-  // Code for any initialization, if required
+function startClocks() {}
+const searchButton = document.querySelector( '.searchButton' );
+const searchInput = document.querySelector( '.searchTerm' );
+const errorMessage = document.createElement( 'div' );
+errorMessage.textContent = 'Please enter a city name!';
+errorMessage.classList.add( 'error-message' );
+errorMessage.style.display = 'none';
+searchButton.parentNode.appendChild( errorMessage );
+searchButton.addEventListener( 'click', () => {
+	const searchTerm = searchInput.value.trim();
+	if ( searchTerm === '' ) {
+		showError();
+		return;
+	}
+	highlightCity( searchTerm );
+	hideError();
+} );
+searchInput.addEventListener( 'keyup', ( event ) => {
+	if ( event.key === 'Enter' ) {
+		event.preventDefault();
+		const searchTerm = searchInput.value.trim();
+		if ( searchTerm === '' ) {
+			showError();
+			return;
+		}
+		highlightCity( searchTerm );
+		hideError();
+	}
+} );
+
+function showError() {
+	errorMessage.style.display = 'block';
 }
 
-// Event listeners for city search
-const searchButton = document.querySelector('.searchButton');
-const searchInput = document.querySelector('.searchTerm');
-const errorDiv = document.createElement('errorDiv');
-errorDiv.textContent = '!Invalid search.';
-errorDiv.style.display = 'none';
-errorDiv.classList.add('error-message');
-
-// Event handler for search button click
-searchButton.addEventListener('click', () => {
-  const searchTerm = searchInput.value.trim();
-  if (searchTerm === '') {
-    errorDiv.style.display = 'block';
-    setTimeout(() => {
-      errorDiv.style.display = 'none'; // Hide the error message after 3 seconds
-    }, 3000);
-    return;
-  }
-
-  highlightCity(searchTerm);
-  errorDiv.style.display = 'none';
-});
-
-// Event handler for Enter key press in the search input field
-searchInput.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // Prevent default form submission behavior
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm === '') {
-      errorDiv.style.display = 'block';
-      setTimeout(() => {
-        errorDiv.style.display = 'none'; // Hide the error message after 3 seconds
-      }, 3000);
-      return;
-    }
-
-    highlightCity(searchTerm);
-    errorDiv.style.display = 'none';
-  }
-});
-
-// List of cities with their IDs and UTC offsets
-const cities = [
-  { id: 'new-york', offset: -4 },       // New York (UTC-4)
-  { id: 'london', offset: 1 },          // London (UTC+1)
-  { id: 'tokyo', offset: 9 },           // Tokyo (UTC+9)
-  { id: 'paris', offset: 2 },           // Paris (UTC+2)
-  { id: 'beijing', offset: 8 },         // Beijing (UTC+8)
-  { id: 'sydney', offset: 10 },         // Sydney (UTC+10)
-  { id: 'mexicocity', offset: -6 },     // Mexico City (UTC-6)
-  { id: 'delhi', offset: -6.5 },        // Delhi (UTC-6.5)
-  { id: 'hongkong', offset: 8 },        // Hong Kong (UTC+8)
-  { id: 'moscow', offset: 3 },          // Moscow (UTC+3)
-];
-
-// Function to update all clocks every second
+function hideError() {
+	errorMessage.style.display = 'none';
+}
+const cities = [ {
+	id: 'new-york',
+	offset: -4
+}, {
+	id: 'london',
+	offset: 1
+}, {
+	id: 'tokyo',
+	offset: 9
+}, {
+	id: 'paris',
+	offset: 2
+}, {
+	id: 'beijing',
+	offset: 8
+}, {
+	id: 'sydney',
+	offset: 10
+}, {
+	id: 'mexicocity',
+	offset: -6
+}, {
+	id: 'delhi',
+	offset: -6.5
+}, {
+	id: 'hongkong',
+	offset: 8
+}, {
+	id: 'moscow',
+	offset: 3
+}, {
+	id: 'shanghai',
+	offset: 8
+}, {
+	id: 'buenosaires',
+	offset: -3
+}, {
+	id: 'mumbai',
+	offset: 5.5
+}, {
+	id: 'istanbul',
+	offset: 3
+}, {
+	id: 'manila',
+	offset: 8
+}, {
+	id: 'lagos',
+	offset: 1
+}, {
+	id: 'seoul',
+	offset: 9
+}, {
+	id: 'saopaulo',
+	offset: -3
+}, {
+	id: 'albasrah',
+	offset: 3
+}, {
+	id: 'losangeles',
+	offset: -7
+}, {
+	id: 'dhaka',
+	offset: 6
+}, {
+	id: 'kiev',
+	offset: 3
+}, {
+	id: 'chicago',
+	offset: -5
+}, {
+	id: 'rome',
+	offset: 2
+}, {
+	id: 'cairo',
+	offset: 3
+} ];
 const updateAllClocks = () => {
-  updateUserLocationClock();
-  cities.forEach((city) => updateClock(city.id, city.offset));
+	updateUserLocationClock();
+	cities.forEach( ( city ) => updateClock( city.id, city.offset ) );
 };
+updateAllClocks();
+setInterval( updateAllClocks, 1000 );
 
-updateAllClocks(); // Update the clocks immediately
-setInterval(updateAllClocks, 1000); // Update the clocks every second (1000 ms)
-
-// Function to handle city click (You can add specific behavior if needed)
-function handleCityClick(cityName) {
-  highlightCity(cityName);
+function handleCityClick( cityName ) {
+	highlightCity( cityName );
 }
-
-// Start the clocks when the page is loaded
 window.onload = () => {
-  startClocks();
-
-  // Add click event listeners to each major city clock
-  const majorCityClocks = document.querySelectorAll('.clock');
-  majorCityClocks.forEach((cityClock) => {
-    cityClock.addEventListener('click', () => {
-      // You can add any desired behavior when clicking the clock here
-      // For example, open a new tab with more information about the city, etc.
-    });
-  });
+	startClocks();
+	const majorCityClocks = document.querySelectorAll( '.clock' );
+	majorCityClocks.forEach( ( cityClock ) => {
+		cityClock.addEventListener( 'click', () => {} );
+	} );
 };
+const cities2 = [ "newYork", "london", "tokyo", "paris", "beijing", "sydney", "mexicocity", "delhi",
+	"hongkong", "moscow"
+];
+cities2.forEach( city => {
+	try {
+		const cityDiv = document.querySelector( `.${city}` );
+		cityDiv.addEventListener( "click", () => {
+			window.location.href = `./CityPages/${city}.html`;
+		} );
+	} catch ( error ) {
+		console.error( `Error adding click event listener for ${city}: ${error.message}` );
+	}
+} );
